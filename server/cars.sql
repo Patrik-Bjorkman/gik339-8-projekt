a786f5087a8c0efc0d12bccbea1ed405f486817b
sabina-lindell <121024588+sabina-lindell@users.noreply.github.com>
2023-12-19 08:41:04 +0100

    html
    uppdaterat html lite till

--- server/cars.sql
+++ server/cars.sql
@@ -3,11 +3,12 @@ CREATE TABLE IF NOT EXISTS cars(
    id        INTEGER  NOT NULL PRIMARY KEY AUTOINCREMENT
   ,regNum VARCHAR(8) NOT NULL
   ,model  VARCHAR(20) NOT NULL
-  ,manuFact  VARCHAR(16) NOT NULL
-  ,manuFactYear INTEGER(4) NOT NULL
+  ,manufact  VARCHAR(16) NOT NULL
+  ,manufactYear INTEGER(4) NOT NULL
   ,color VARCHAR(6) NOT NULL
+  ,fuel VARCHAR(6) NOT NULL
 );
-INSERT INTO cars(id,regNum,model,manuFact,manuFactYear,color) VALUES (1,'ABC123','Ursus S','Lamborghini','2023','red');
-INSERT INTO cars(id,regNum,model,manuFact,manuFactYear,color) VALUES (2,'HGE456','LS','Lexus','2022','White');
+INSERT INTO cars(id,regNum,model,manuFact,manufactYear,color,fuel) VALUES (1,'ABC123','Ursus S','Lamborghini','2023','Red','Bensin');
+INSERT INTO cars(id,regNum,model,manuFact,manufactYear,color,fuel) VALUES (2,'HGE456','LS','Lexus','2022','Vit','miljöbränsel/Hybrid');
 
 select * from cars;
